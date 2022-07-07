debe1ce9dfed3be35f2f13df9e3e5350824691ee
peter-bear <2388152240@qq.com>
2022-07-06 22:59:06 -0400

    finish node simple server follow along

---
+++ FollowAlongs/Node/SimpleServers/noExpress.js
@@ -0,0 +1,66 @@
+let abcTracker = 0;
+let totalTracker = 0;
+
+const http = require('http');
+const _ = require("underscore");
+
+const abcHandler = function (request, response) {
+    response.write('<h1>ABC</h1>');
+};
+
+const xyzHandler = function (request, response) {
+    response.write('<!doctype html>\n');
+    response.write('<html>\n<head>\n');
+    response.write('<title>Dice Roller</title>\n');
+    response.write('</head>\n<body>\n');
+    response.write('<h1>Hello World!</h1>');
+    response.write('<div>abcTracker = '+abcTracker+' </div>');
+    response.write('<div>totalTracker = '+totalTracker+' </div>');
+    for(let i=0;i<5;i++){
+        let randNum = _.random(1, 6);
+        response.write('<p>'+randNum+'</p>');
+    }
+
+
+
+    response.write('</body>\n</html>\n');
+};
+
+
+const faviconHandler = function (request, response) {
+    // console.log('favicon requested');
+    response.writeHead(204, {'Content-Type':'image/x-icon'});
+    response.end();
+};
+
+const mainHandler = function (request, response) {
+    if(request.url == '/favicon.ico'){
+        faviconHandler(request, response);
+        return;
+    }
+
+    response.statusCode = 200;
+    response.setHeader('Content-Type','text/html');
+    console.log(request.url);
+
+    if(request.url == '/abc'){
+        abcTracker++;
+        abcHandler(request, response);
+    }
+
+    let re =/^\/xyz.*/;
+    if(re.test(request.url)){
+        xyzHandler(request, response);
+    }
+
+    totalTracker++;
+    response.end();
+};
+
+const server = http.createServer(mainHandler);
+server.listen(3000, (err) => {
+    if(err){
+        console.log("Error: ", err);
+    }
+    console.log("Listening on port 3000");    
+});
